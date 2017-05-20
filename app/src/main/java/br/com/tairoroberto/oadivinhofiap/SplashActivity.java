09307817d0475c132953d5c1539c37d91b430a7f
tairoroberto <tairoroberto@hotmail.com>
2017-05-20 11:57:11 -0300

    Ajuste app

--- app/src/main/java/br/com/tairoroberto/oadivinhofiap/SplashActivity.java
+++ app/src/main/java/br/com/tairoroberto/oadivinhofiap/SplashActivity.java
@@ -1,22 +1,45 @@
 package br.com.tairoroberto.oadivinhofiap;
 
-import android.animation.Animator;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private static final long SPLASH_DISPLAY_LENGTH = 3500;
     private ImageView imageViewSplash;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        carregar();
+    }
 
-        imageViewSplash = (ImageView) findViewById(R.id.imageViewSplash);
+    private void carregar() {
+        Animation anim = AnimationUtils.loadAnimation(this,
+                R.anim.animacao_splash);
+        anim.reset();
+        //Pegando o nosso objeto criado no layout
+        ImageView iv = (ImageView) findViewById(R.id.imageViewSplash);
+        if (iv != null) {
+            iv.clearAnimation();
+            iv.startAnimation(anim);
+        }
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Após o tempo definido irá executar a próxima tela
+                Intent intent = new Intent(SplashActivity.this,
+                        MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
     }
 }
